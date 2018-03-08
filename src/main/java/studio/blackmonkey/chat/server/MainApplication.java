e439f8c87b797fa4772eb336f8230febc82730e4
Oscar Cai <blackmuffus@yahoo.com>
2018-03-08 17:04:10 +0800

    Rename package and main class

--- src/main/java/studio/blackmonkey/chat/server/MainApplication.java
+++ src/main/java/studio/blackmonkey/chat/server/MainApplication.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package studio.blackmonkey.chat.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 }
